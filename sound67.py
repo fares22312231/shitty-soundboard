d0d2da616eec7ffd4949c5a43991bbf011cc630e
fares22312231 <faresalattal3@gmail.com>
2025-11-08 13:44:48 +0300

    fixed sound67.py

--- sound67.py
+++ sound67.py
@@ -7,31 +7,31 @@ from PIL import Image, ImageTk
 pygame.mixer.init()
 
 def on_click():
-    pygame.mixer.music.load("/home/fschool/Downloads/dry-fart.mp3")
+    pygame.mixer.music.load("change this to were your mp3 files are")
     pygame.mixer.music.play() 
 
 
 
 def on_click3():
-    pygame.mixer.music.load("/home/fschool/Downloads/fahhhhhhhhhhhhhhh.mp3")
+    pygame.mixer.music.load("change this to were your mp3 files are")
     pygame.mixer.music.play()
 
 def on_click4():
-    pygame.mixer.music.load("/home/fschool/Downloads/[2188317559] SlipONBLEAT - yes_chef.mp3")
+    pygame.mixer.music.load("change this to were your mp3 files are")
     pygame.mixer.music.play()
 
 def on_click5():
-    pygame.mixer.music.load("/home/fschool/Downloads/wombo-combo_2.mp3")
+    pygame.mixer.music.load("change this to were your mp3 files are")
     pygame.mixer.music.play()
 
 def on_click6():
-    pygame.mixer.music.load("/home/fschool/Downloads/wow-mlg-sound-effect.mp3")
+    pygame.mixer.music.load("change this to were your mp3 files are")
     pygame.mixer.music.play()
 
 root = tk.Tk()
 root.title("soundboard")
 root.geometry("1440x900")
-bg_image = Image.open("/home/fschool/Downloads/SON.jpeg")  
+bg_image = Image.open("change this to what you want the background to be make it the directory leading to the image")  
 bg_image = bg_image.resize((1440, 900))  
 bg_photo = ImageTk.PhotoImage(bg_image)
 
@@ -42,22 +42,22 @@ bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 bg_label.image = bg_photo
 
 
-button = tk.Button(root, text="fart", command=on_click)
+button = tk.Button(root, text="change this", command=on_click)
 button.pack()
 
 
 
-buttom3 = tk.Button(root, text="FAH", command=on_click3)
+buttom3 = tk.Button(root, text="change this", command=on_click3)
 buttom3.pack()
 
-button4 = tk.Button(root, text="yes_chef", command=on_click4)
+button4 = tk.Button(root, text="change this", command=on_click4)
 button4.pack()
 
-button5 = tk.Button(root, text="WOMBOCOMBO", command=on_click5)
+button5 = tk.Button(root, text="change this", command=on_click5)
 button5.pack()
 
 
-button6 = tk.Button(root, text="wow.mlg", command=on_click6)
+button6 = tk.Button(root, text="change this", command=on_click6)
 button6.pack()
 
 
